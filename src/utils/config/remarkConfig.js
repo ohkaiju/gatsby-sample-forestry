fbee649b18b01c01351b1b0686854f772d9928f3
ohkaiju <lcmte.ju@gmail.com>
2019-04-14 13:44:31 +0200

    text field exp

--- src/utils/config/remarkConfig.js
+++ src/utils/config/remarkConfig.js
@@ -2,32 +2,27 @@ module.exports = {
   resolve: `gatsby-transformer-remark`,
   options: {
     plugins: [
+/*       {
+        resolve: "gatsby-remark-normalize-paths"
+      }, */
       {
-        resolve: `gatsby-remark-relative-images`,
+        resolve: `gatsby-remark-relative-images`
       },
-      //`gatsby-remark-normalize-paths`,
       {
         resolve: `gatsby-remark-images`,
         options: {
           maxWidth: 1200,
           linkImagesToOriginal: true,
           quality: 100,
-          withWebp: true,
-        },
+          withWebp: true
+        }
       },
-      /* {
-        resolve: `gatsby-remark-responsive-iframe`,
-        options: {
-          wrapperStyle: `margin-bottom: 1.0725rem`,
-        },
-      }, */
       {
         resolve: "gatsby-remark-external-links",
         options: {
-          target: "_blank",
-        },
-      },
-      //"gatsby-remark-copy-linked-files",
-    ],
-  },
+          target: "_blank"
+        }
+      }
+    ]
+  }
 }
