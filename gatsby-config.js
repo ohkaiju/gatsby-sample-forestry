c89e5cf7f58f73809a979a8495f7bc133ea45d05
ohkaiju <lcmte.ju@gmail.com>
2019-04-15 16:38:18 +0200

    remove plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          `gatsby-remark-relative-images`,
+          //`gatsby-remark-relative-images`,
           `gatsby-remark-copy-linked-files`,
           {
             resolve: `gatsby-remark-images`,
